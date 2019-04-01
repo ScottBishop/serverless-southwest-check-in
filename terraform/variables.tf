edf10f05cc077b9bdc9b13e9b47456bd28c8afab
scott.bishop <scott.bishop@workday.com>
2019-04-01 08:52:41 -0700

    Use tfvars file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,13 @@
 variable "domains" {
   type        = "list"
   description = "List of domains/subdomains to create the service under. WARNING: The MX records for this domain will be overwritten!"
-  default     = ["serverless-southwest-check-in.com"]
+}
+
+variable "recipients" {
+  type        = "list"
+  description = "List of email address patterns to deliver to, e.g. `checkin@example.com`, `example.com`, `subdomain.example.com`."
 }
 
 variable "admin_email" {
   description = "This email address will be used for alerts and BCCs on confirmations"
-  default     = "scottbishop70@gmail.com"
 }
